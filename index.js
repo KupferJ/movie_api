620c582416931a560a62bafa63ab7ef196070217
KupferJ <jorgekupfer@gmx.de>
2022-04-02 11:27:39 +0200

    cors

--- index.js
+++ index.js
@@ -27,8 +27,14 @@ app.use(morgan('common'));
 
 //use CORS
 const cors = require('cors');
-app.use(cors({credentials: true, origin: true}));
+app.use(cors());
 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
 
 //import the "auth.js" file
 let auth = require('./auth.js');
